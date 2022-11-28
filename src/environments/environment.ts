003a583b529d128928ce648c110b4da96773adf6
waynegakuo <waynejotham@gmail.com>
2022-11-28 12:22:43 +0300

    installs(project): install Firebase and setup Firebase hosting and GitHub Actions CI/CD

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCWznFc377xmL1gYSDWN8qMhatWdQLVzIU",
+    authDomain: "deezermusicapp.firebaseapp.com",
+    projectId: "deezermusicapp",
+    storageBucket: "deezermusicapp.appspot.com",
+    messagingSenderId: "841228222713",
+    appId: "1:841228222713:web:2ab2e9d48c84d6f8b5e228",
+    measurementId: "G-3T87MF54PZ"
+  }
 };
 
 /*
