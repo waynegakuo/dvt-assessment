ab2acb99ebe2175c57b201ad93ed9191593c746c
Wayne Gakuo <30318171+waynegakuo@users.noreply.github.com>
2022-11-28 12:32:09 +0300

    installs(project): Tailwind CSS & Firebase Setup (#2)
    * installs(project): tailwind css
    
    * installs(project): install Firebase and setup Firebase hosting and GitHub Actions CI/CD
    
    * installs(project): fix issue of builds
    
    * installs(project): fix issue of builds

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCWznFc377xmL1gYSDWN8qMhatWdQLVzIU",
+    authDomain: "deezermusicapp.firebaseapp.com",
+    projectId: "deezermusicapp",
+    storageBucket: "deezermusicapp.appspot.com",
+    messagingSenderId: "841228222713",
+    appId: "1:841228222713:web:2ab2e9d48c84d6f8b5e228",
+    measurementId: "G-3T87MF54PZ"
+  }
 };
