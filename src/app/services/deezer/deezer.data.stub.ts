eff72bd158662fb39d8a5b12409fb3140e9b9c46
waynegakuo <waynejotham@gmail.com>
2022-11-29 18:14:20 +0300

    feature(project): landing page test

---
+++ src/app/services/deezer/deezer.data.stub.ts
@@ -0,0 +1,30 @@
+import { Artist } from "src/app/models/Artist/artist.model";
+
+export const ARTIST_DATA_STUB: Artist[] = [
+  {
+    id: 1,
+    name: 'John Doe',
+    link: 'string',
+    picture: 'https://www.deezer.com/artist/1',
+    picture_small: 'https://api.deezer.com/artist/1/image',
+    picture_medium: 'https://e-cdns-images.dzcdn.net/images/artist/895c65537c74c2014b459f73e84bccb0/56x56-000000-80-0-0.jpg',
+    picture_big: 'https://e-cdns-images.dzcdn.net/images/artist/895c65537c74c2014b459f73e84bccb0/500x500-000000-80-0-0.jpg',
+    picture_xl: 'https://e-cdns-images.dzcdn.net/images/artist/895c65537c74c2014b459f73e84bccb0/1000x1000-000000-80-0-0.jpg',
+    nb_album: 30,
+    nb_fan: 6824734,
+    radio: true
+  },
+  {
+    id: 2,
+    name: 'Mary Jane Doe',
+    link: 'string',
+    picture: 'https://www.deezer.com/artist/1',
+    picture_small: 'https://api.deezer.com/artist/1/image',
+    picture_medium: 'https://e-cdns-images.dzcdn.net/images/artist/895c65537c74c2014b459f73e84bccb0/56x56-000000-80-0-0.jpg',
+    picture_big: 'https://e-cdns-images.dzcdn.net/images/artist/895c65537c74c2014b459f73e84bccb0/500x500-000000-80-0-0.jpg',
+    picture_xl: 'https://e-cdns-images.dzcdn.net/images/artist/895c65537c74c2014b459f73e84bccb0/1000x1000-000000-80-0-0.jpg',
+    nb_album: 80,
+    nb_fan: 10554834,
+    radio: true
+  },
+]
